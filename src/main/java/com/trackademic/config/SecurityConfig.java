d757387b7ecdc1c90a640c3f965fc3b76091d99b
juancamilocorralesosvath <juancamilocorra@gmail.com>
2025-05-20 10:24:35 -0500

    added home controller

---
+++ src/main/java/com/trackademic/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.trackademic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    // we’re allowing unrestricted access to all endpoints.
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(expressionInterceptUrlRegistry ->
+                expressionInterceptUrlRegistry
+                        .anyRequest()
+                        .permitAll()
+        ).csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
